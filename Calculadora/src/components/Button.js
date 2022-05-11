024f16dd342fda76d4998b65c01af2a34b1878d4
Maria Baia <mariabaia@MacBook-Pro-de-Maria.local>
2022-05-11 11:36:13 -0400

    feat: projeto calculadora refactored

--- Calculadora/src/components/Button.js
+++ Calculadora/src/components/Button.js
@@ -1,42 +1,43 @@
-import React from 'react'
-import {
-    StyleSheet,
-    Text,
-    Dimensions,
-    TouchableHighlight
-} from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, Dimensions, TouchableHighlight} from 'react-native';
 
 const styles = StyleSheet.create({
-    button: {
-        fontSize: 40,
-        height: Dimensions.get('window').width / 4,
-        width: Dimensions.get('window').width / 4,
-        padding: 20,
-        backgroundColor: '#f0f0f0',
-        textAlign: 'center',
-        borderWidth: 1,
-        borderColor: '#888',
-    },
-    operationButton: {
-        color: '#fff',
-        backgroundColor: '#fa8231',
-    },
-    buttonDouble: {
-        width: (Dimensions.get('window').width / 4) * 2,
-    },
-    buttonTriple: {
-        width: (Dimensions.get('window').width / 4) * 3,
-    }
-})
+  button: {
+    fontSize: 40,
+    height: Dimensions.get('window').width / 4,
+    width: Dimensions.get('window').width / 4,
+    padding: 20,
+    backgroundColor: '#fcfcfc',
+    textAlign: 'center',
+    borderWidth: 1,
+    borderColor: '#02111b',
+  },
+  operationButton: {
+    color: '#3f4045',
+    backgroundColor: '#5d737e',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
+});
 
 export default props => {
-    const stylesButton = [styles.button]
-    if (props.double) stylesButton.push(styles.buttonDouble)
-    if (props.triple) stylesButton.push(styles.buttonTriple)
-    if (props.operation) stylesButton.push(styles.operationButton)
-    return (
-        <TouchableHighlight onPress={() => props.onClick(props.label)}>
-            <Text style={stylesButton}>{props.label}</Text>
-        </TouchableHighlight>
-    )
-}
+  const stylesButton = [styles.button];
+  if (props.double) {
+    stylesButton.push(styles.buttonDouble);
+  }
+  if (props.triple) {
+    stylesButton.push(styles.buttonTriple);
+  }
+  if (props.operation) {
+    stylesButton.push(styles.operationButton);
+  }
+  return (
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
+    </TouchableHighlight>
+  );
+};
--- Calculadora/src/components/Display.js
+++ Calculadora/src/components/Display.js
@@ -1,26 +1,26 @@
-import React from 'react'
-import {
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const styles = StyleSheet.create({
-    display: {
-        flex: 1,
-        padding: 20,
-        justifyContent: 'center',
-        backgroundColor: 'rgba(0,0,0,0.6)',
-        alignItems: 'flex-end',
-    },
-    displayValue: {
-        fontSize: 60,
-        color: '#fff',
-    }
-})
+  display: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+    backgroundColor: '#30292f',
+    alignItems: 'flex-end',
+  },
+  displayValue: {
+    fontSize: 60,
+    color: '#5d737e',
+  },
+});
 
-export default props => 
+export default ({value}) => {
+  return (
     <View style={styles.display}>
-        <Text style={styles.displayValue}
-            numberOfLines={1}>{props.value}</Text>
+      <Text style={styles.displayValue} numberOfLines={1}>
+        {value}
+      </Text>
     </View>
+  );
+};
